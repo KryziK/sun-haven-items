8762ee1048bcc8786ae01b3ba7e3b2c8dc74e4f8
KryziK <kryzik@kryzik.com>
2023-04-27 19:54:39 -0400

    Add link to mods

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,10 +1,11 @@
-import { Container, Divider, Typography } from "@mui/material";
+import { Container, Divider, Link, Typography } from "@mui/material";
 
 const Footer = () => {
   return (
     <Container maxWidth="xl" sx={{ display: "flex", flexDirection: "column", alignItems: "center", flexGrow: 1, paddingTop: 5 }}>
       <Divider flexItem variant="middle" />
-      <Typography align="center" color="text.secondary" maxWidth="lg">
+      <Typography>Check out my mods over at <Link color="#d98f40" href="https://www.nexusmods.com/sunhaven/users/31196780?tab=user+files">https://www.nexusmods.com/sunhaven/users/31196780</Link>!</Typography>
+      <Typography align="center" color="text.secondary" variant="caption" maxWidth="lg">
         This website and its contents are neither endorsed nor affiliated with Sun Haven or its creator, Pixel Sprout Studios.<br />
         This is purely a third-party convenience tool and hobby project.
       </Typography>
