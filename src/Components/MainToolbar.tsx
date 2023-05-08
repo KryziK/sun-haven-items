c6b5030ac63fd308b52b8a5291b4e12973b04450
KryziK <kryzik@kryzik.com>
2023-05-08 15:19:58 -0400

    pixelated bukkake

--- src/Components/MainToolbar.tsx
+++ src/Components/MainToolbar.tsx
@@ -1,5 +1,5 @@
 import { Search } from "@mui/icons-material";
-import { Container, FormControlLabel, Grid, InputAdornment, Switch, TextField, Typography } from "@mui/material";
+import { Container, FormControlLabel, Grid, InputAdornment, Switch, TextField, Theme, Typography, useMediaQuery } from "@mui/material";
 import { useEffect, useState } from "react";
 
 const SearchIcon = () => <InputAdornment position="start"><Search /></InputAdornment>;
@@ -12,6 +12,7 @@ const MainToolbar = ({ shown, results, updateQuery, compact, setCompact }: {
   setCompact: React.Dispatch<React.SetStateAction<boolean>>
 }) => {
   const [searchText, setSearchText] = useState("");
+  const isSmallScreen = useMediaQuery<Theme>(theme => theme.breakpoints.down("sm"));
 
   useEffect(() => {
     updateQuery(searchText);
@@ -19,7 +20,7 @@ const MainToolbar = ({ shown, results, updateQuery, compact, setCompact }: {
 
   return (
     <Container maxWidth="xl" sx={{ flexGrow: 1, paddingY: 2 }}>
-      <Grid container spacing={2} width="100%">
+      <Grid container spacing={isSmallScreen ? 1 : 2} width="100%">
         <Grid item xs={6} ml={3}>
           <TextField
             autoFocus
@@ -34,9 +35,9 @@ const MainToolbar = ({ shown, results, updateQuery, compact, setCompact }: {
         <Grid item xs>
           <FormControlLabel control={<Switch checked={compact} onChange={e => setCompact(e.target.checked)} />} label="Compact" />
         </Grid>
-        <Grid item xs></Grid>
-        <Grid item xs={2} display="flex" alignItems="center" justifyContent="end">
-          <Typography variant="h6">{shown} / {results}</Typography>
+        {!isSmallScreen && <Grid item xs></Grid>}
+        <Grid item xs={"auto"} display="flex" alignItems="center" justifyContent="end">
+          <Typography variant={isSmallScreen ? "body1" : "h6"}>{shown}</Typography>
         </Grid>
       </Grid>
     </Container>
